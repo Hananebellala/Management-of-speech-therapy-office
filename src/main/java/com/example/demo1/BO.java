fdbeec50760903666a949a515242eb804a664e5f
Hananebellala <hananebellala17@gmail.com>
2024-05-25 01:45:02 +0100

    Logique

---
+++ src/main/java/com/example/demo1/BO.java
@@ -0,0 +1,59 @@
+package com.example.demo1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BO {
+    protected Anamnese anamnese;
+    protected EpreuveClinique epreuve;
+    protected Diagnostic diagnostic;
+    protected String projetTherapeutique;
+    protected  static int id;
+
+    public BO(Anamnese anam, EpreuveClinique epreuve, Diagnostic diag,String projetTherapeutique){
+        this.anamnese=anam;
+        this.epreuve=epreuve;
+        this.diagnostic=diag;
+        this.projetTherapeutique=projetTherapeutique;
+        this.id=0;
+
+    }
+
+    private Map<Integer, Patient> patients;
+
+    public BO() {
+        this.patients = new HashMap<>();
+    }
+
+    public void addPatient(Patient patient) {
+        patients.put(id, patient);
+        id++;
+    }
+
+    public Patient getPatient(int id) {
+        return patients.get(id);
+    }
+
+    public void updateEpreuveClinique(EpreuveClinique epreuveClinique) {
+        this.epreuve=epreuveClinique;
+    }
+
+    public void setAnamnese(Anamnese anamnese) {
+        this.anamnese=anamnese;
+    }
+
+    public void addDiagnostic(Diagnostic diagnostic) {
+        this.diagnostic=diagnostic;
+        System.out.println("Diagnostic added: " + diagnostic.getNom() + ", " + diagnostic.getCategorie());
+    }
+
+    public void saveProjetTherapeutique(String demarche) {
+        this.projetTherapeutique = demarche;
+        System.out.println("Projet Therapeutique saved: " + demarche);
+    }
+
+    // Getter for projetTherapeutique if needed
+    public String getProjetTherapeutique() {
+        return projetTherapeutique;
+    }
+}
---
+++ src/main/java/com/example/demo1/GestionQCMController.java
@@ -0,0 +1,79 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class GestionQCMController {
+
+    @FXML
+    private Button Ajouter;
+
+    @FXML
+    private TextField Proposition;
+
+    @FXML
+    private TextField Question;
+
+    @FXML
+    private TextField Reponses;
+
+    @FXML
+    private Button Suprimer;
+
+    @FXML
+    private Label ans1;
+
+    @FXML
+    private Label ans2;
+
+    @FXML
+    private Label ans3;
+
+    @FXML
+    private Label ans4;
+
+    @FXML
+    private Label ans5;
+
+    @FXML
+    private Label ans6;
+
+    @FXML
+    private Label prop1;
+
+    @FXML
+    private Label prop2;
+
+    @FXML
+    private Label prop3;
+
+    @FXML
+    private Label prop4;
+
+    @FXML
+    private Label prop5;
+
+    @FXML
+    private Label prop6;
+
+    @FXML
+    private Label qst1;
+
+    @FXML
+    private Label qst2;
+
+    @FXML
+    private Label qst3;
+
+    @FXML
+    private Label qst4;
+
+    @FXML
+    private Label qst5;
+
+    @FXML
+    private Label qst6;
+
+}
